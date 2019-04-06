a93962a2d2977a47cffe9af87202050d38414f71
stojmenovic <ivan.stojmenovic@gmail.com>
2019-04-06 15:20:49 +0200

    06/04/2019

---
+++ Selenium/src/start/Simple_Menu_Start.java
@@ -0,0 +1,14 @@
+package start;
+
+import utility.Input;
+
+public class Simple_Menu_Start {
+
+	public static void main(String[] args) throws Exception {
+		/*
+		 * Calling this method, program is starting with simple user menu, where user is
+		 * asked to choose action. Each action is calling its own method
+		 */
+		Input.display_menu();
+	}
+}
