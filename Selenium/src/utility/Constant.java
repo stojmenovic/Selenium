3a017e092fea4806f2aa8774b4ce8dac9ca31672
stojmenovic <ivan.stojmenovic@gmail.com>
2019-04-06 12:05:35 +0200

    05/04/2019

--- Selenium/src/utility/Constant.java
+++ Selenium/src/utility/Constant.java
@@ -2,7 +2,7 @@ package utility;
 
 public class Constant {
 	/* Defining of final values for Constants (website,Excel path...) */
-	public static final String URL = "http://localhost/izlet/";
+	public static final String URL = "https://izlet.000webhostapp.com/izlet/";
 	public static final String SHEET_NAME = "Registracija";
 	public static final String SHEET_NAME2 = "Tekst";
 	public static final String Path_TestData = "C:\\Users\\stojmenovic\\Desktop\\Prilog\\";
